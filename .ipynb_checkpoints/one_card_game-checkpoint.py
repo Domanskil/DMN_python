eb04fb2a0b6f1b142db750a8d3ee18dc5c2ef48d
doman <lukaszdoman86@gmail.com>
2026-07-01 05:45:49 +0200

    commit

--- .ipynb_checkpoints/one_card_game-checkpoint.py
+++ .ipynb_checkpoints/one_card_game-checkpoint.py
@@ -6,16 +6,119 @@
 import karty
 import gry
 
-class BJ_Card(karty.Card):
-    """ Karta do blackjacka. """
-    ACE_VALUE = 1
+class Game_Card(karty.Card):
+
 
     @property
     def value(self):
-        if self.is_face_up:
-            v = BJ_Card.RANKS.index(self.rank) + 1
-            if v > 10:
-                v = 10
-        else:
-            v = None
+        v = Game_Card.RANKS.index(self.rank) + 1
+        if v == 1:
+            v = 14
+        
         return v
+
+class Game_Deck(karty.Deck):
+    
+    def populate(self):
+        for suit in Game_Card.SUITS: 
+            for rank in Game_Card.RANKS: 
+                self.cards.append(Game_Card(rank, suit))
+
+
+
+class Player(karty.Hand):  
+    
+    
+    def __init__(self, name):
+        super(Player, self).__init__()
+        self.name = name
+    
+    def __str__(self):
+        rep = self.name + ":\t" + super(Player, self).__str__()  
+               
+        return rep
+
+    @property
+    def total(self):
+        t = self.cards[0].value
+
+        return t
+    
+    def lose(self):
+        print(self.name, "przegrywa.")
+
+    def win(self):
+        print(self.name, "wygrywa.")
+
+    def push(self):
+        print(self.name, "remisuje.")
+
+class Game(object):
+    
+    def __init__(self, names):      
+        self.players = []
+        for name in names:
+            player = Player(name)
+            self.players.append(player)
+
+        self.deck = Game_Deck()
+        self.deck.populate()
+        self.deck.shuffle()
+
+
+    def play(self):
+        
+        self.deck.deal(self.players, per_hand = 1)
+           
+        
+        for player in self.players:
+                print(player)
+                print(player.total)
+
+            
+        # while len(self.players) > 1:
+            
+        #     #self.players.sort(player.card.value, reverse = True)
+            
+        #     for player in self.players:
+        #         print(player)
+        #         print(player.card.value)
+
+
+            
+            #     self.players.remove(player)
+            #     for player_1 in players:
+            #         if player.card.value >
+            
+
+
+
+#test
+        if len(self.deck.cards) < 26:
+            self.shoe.populate()
+            self.shoe.shuffle()
+            self.deck.cards += self.shoe.cards
+            self.shoe.clear()
+
+
+
+def main():
+    print("\t\tWitaj w grze 'One Card Game'!\n")
+    
+    names = []
+    number = gry.ask_number("Podaj liczbę graczy (1 - 7): ", low = 1, high = 8)
+    for i in range(number):
+        name = input("Wprowadź nazwę gracza: ")
+        names.append(name)
+    print()
+        
+    game = Game(names)
+
+    again = None
+    while again != "n":
+        game.play()
+        again = gry.ask_yes_no("\nCzy chcesz zagrać ponownie?: ")
+
+
+main()
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
--- one_card_game.py
+++ one_card_game.py
@@ -11,21 +11,40 @@ class Game_Card(karty.Card):
 
     @property
     def value(self):
-        if self.is_face_up:
-            v = BJ_Card.RANKS.index(self.rank) + 1
-            if v == 1:
-                v = 14
+        v = Game_Card.RANKS.index(self.rank) + 1
+        if v == 1:
+            v = 14
         
         return v
 
-
 class Game_Deck(karty.Deck):
     
-class Game_Hand(karty.Hand):
+    def populate(self):
+        for suit in Game_Card.SUITS: 
+            for rank in Game_Card.RANKS: 
+                self.cards.append(Game_Card(rank, suit))
 
-class Player(Game_Hand):
+
+
+class Player(karty.Hand):  
     
-     def lose(self):
+    
+    def __init__(self, name):
+        super(Player, self).__init__()
+        self.name = name
+    
+    def __str__(self):
+        rep = self.name + ":\t" + super(Player, self).__str__()  
+               
+        return rep
+
+    @property
+    def total(self):
+        t = self.cards[0].value
+
+        return t
+    
+    def lose(self):
         print(self.name, "przegrywa.")
 
     def win(self):
@@ -42,16 +61,64 @@ class Game(object):
             player = Player(name)
             self.players.append(player)
 
-        self.deck = Game_Deck
+        self.deck = Game_Deck()
         self.deck.populate()
         self.deck.shuffle()
 
 
     def play(self):
-        # rozdaj każdemu początkowe dwie karty
+        
         self.deck.deal(self.players, per_hand = 1)
            
-        for player in self.players:
-            print(player)
-
         
+        for player in self.players:
+                print(player)
+                print(player.total)
+
+            
+        # while len(self.players) > 1:
+            
+        #     #self.players.sort(player.card.value, reverse = True)
+            
+        #     for player in self.players:
+        #         print(player)
+        #         print(player.card.value)
+
+
+            
+            #     self.players.remove(player)
+            #     for player_1 in players:
+            #         if player.card.value >
+            
+
+
+
+#test
+        if len(self.deck.cards) < 26:
+            self.shoe.populate()
+            self.shoe.shuffle()
+            self.deck.cards += self.shoe.cards
+            self.shoe.clear()
+
+
+
+def main():
+    print("\t\tWitaj w grze 'One Card Game'!\n")
+    
+    names = []
+    number = gry.ask_number("Podaj liczbę graczy (1 - 7): ", low = 1, high = 8)
+    for i in range(number):
+        name = input("Wprowadź nazwę gracza: ")
+        names.append(name)
+    print()
+        
+    game = Game(names)
+
+    again = None
+    while again != "n":
+        game.play()
+        again = gry.ask_yes_no("\nCzy chcesz zagrać ponownie?: ")
+
+
+main()
+input("\n\nAby zakończyć program, naciśnij klawisz Enter.")
