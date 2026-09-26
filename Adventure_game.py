85c93491ccf22e33aaceb41fba5fa33a6effb022
domanskil <lukaszdoman86@gmail.com>
2026-09-26 11:19:05 +0200

    commit

--- Adventure_game.py
+++ Adventure_game.py
@@ -5,7 +5,7 @@ app = marimo.App(width="columns")
 
 
 @app.cell
-def _(i, lg, locations, p1, p2, p3, p4):
+def _(i, locations):
     # # Simple game about traveling to different places. 
     # I want to make a grid of random place names, like a chess board. Than maybe a Dice roll for distance and choice of direction. 
     # Location will have random points 1-3, and after five rolls the player with most points wins. 
@@ -42,55 +42,6 @@ def _(i, lg, locations, p1, p2, p3, p4):
 
 
     """
-
-
-
-    # Class Game (turn counter, player and score display), Locations (all shit happen here), Grid (just display), Player ( move -> locations update, score count)
-
-
-    # class Grid(object):   
-
-    #     def __init__(self, locations = None):
-    #         self._locations = locations
-
-    #     @property
-    #     def locations(self):
-
-    #         return self._locations
-
-    #     @locations.setter
-    #     def locations(self, locations):
-    #         self._locations = locations
-    #         return self._location
-
-    #     def __str__(self):
-
-    #         """define alias for function get() to fit the commands on screen :)"""
-
-    #         #locations update!!!
-
-    #         lg = self.locations.get 
-
-    #         self.grid = (f"""
-                # |- - - -|- - - -|- - - -|- - - -|- - - -|
-                # |{lg('1a')[0]}-    -|{lg('1b')[0]}-    -|{lg('1c')[0]}-    -|{lg('1d')[0]}-    -|{lg('1e')[0]}-    -|
-                # |{lg('1a')[1]} {lg('1a')[2]} {lg('1a')[3]} {lg('1a')[4]}|{lg('1b')[1]} {lg('1b')[2]} {lg('1b')[3]} {lg('1b')[4]}|{lg('1c')[1]} {lg('1c')[2]} {lg('1c')[3]} {lg('1c')[4]}|{lg('1d')[1]} {lg('1d')[2]} {lg('1d')[3]} {lg('1d')[4]}|{lg('1e')[1]} {lg('1e')[2]} {lg('1e')[3]} {lg('1e')[4]}|
-                # |- - - -|- - - -|- - - -|- - - -|- - - -|
-                # |{lg('2a')[0]}-    -|{lg('2b')[0]}-    -|{lg('2c')[0]}-    -|{lg('2d')[0]}-    -|{lg('2e')[0]}-    -|
-                # |{lg('2a')[1]} {lg('2a')[2]} {lg('2a')[3]} {lg('2a')[4]}|{lg('2b')[1]} {lg('2b')[2]} {lg('2b')[3]} {lg('2b')[4]}|{lg('2c')[1]} {lg('2c')[2]} {lg('2c')[3]} {lg('2c')[4]}|{lg('2d')[1]} {lg('2d')[2]} {lg('2d')[3]} {lg('2d')[4]}|{lg('2e')[1]} {lg('2e')[2]} {lg('2e')[3]} {lg('2e')[4]}|
-                # |- - - -|- - - -|- - - -|- - - -|- - - -|
-                # |{lg('3a')[0]}-    -|{lg('3b')[0]}-    -|{lg('3c')[0]}-    -|{lg('3d')[0]}-    -|{lg('3e')[0]}-    -|
-                # |{lg('3a')[1]} {lg('3a')[2]} {lg('3a')[3]} {lg('3a')[4]}|{lg('3b')[1]} {lg('3b')[2]} {lg('3b')[3]} {lg('3b')[4]}|{lg('3c')[1]} {lg('3c')[2]} {lg('3c')[3]} {lg('3c')[4]}|{lg('3d')[1]} {lg('3d')[2]} {lg('3d')[3]} {lg('3d')[4]}|{lg('3e')[1]} {lg('3e')[2]} {lg('3e')[3]} {lg('3e')[4]}|
-                # |- - - -|- - - -|- - - -|- - - -|- - - -|
-                # |{lg('4a')[0]}-    -|{lg('4b')[0]}-    -|{lg('4c')[0]}-    -|{lg('4d')[0]}-    -|{lg('4e')[0]}-    -|
-                # |{lg('4a')[1]} {lg('4a')[2]} {lg('4a')[3]} {lg('4a')[4]}|{lg('4b')[1]} {lg('4b')[2]} {lg('4b')[3]} {lg('4b')[4]}|{lg('4c')[1]} {lg('4c')[2]} {lg('4c')[3]} {lg('4c')[4]}|{lg('4d')[1]} {lg('4d')[2]} {lg('4d')[3]} {lg('4d')[4]}|{lg('4e')[1]} {lg('4e')[2]} {lg('4e')[3]} {lg('4e')[4]}|
-                # |- - - -|- - - -|- - - -|- - - -|- - - -|
-                # |{lg('5a')[0]}-    -|{lg('5b')[0]}-    -|{lg('5c')[0]}-    -|{lg('5d')[0]}-    -|{lg('5e')[0]}-    -|
-                # |{lg('5a')[1]} {lg('5a')[2]} {lg('5a')[3]} {lg('5a')[4]}|{lg('5b')[1]} {lg('5b')[2]} {lg('5b')[3]} {lg('5b')[4]}|{lg('5c')[1]} {lg('5c')[2]} {lg('5c')[3]} {lg('5c')[4]}|{lg('5d')[1]} {lg('5d')[2]} {lg('5d')[3]} {lg('5d')[4]}|{lg('5e')[1]} {lg('5e')[2]} {lg('5e')[3]} {lg('5e')[4]}|
-                # |- - - -|- - - -|- - - -|- - - -|- - - -|
-    #             """)
-
-    #         return(self.grid)
 
 
 
@@ -140,16 +91,19 @@ def _(i, lg, locations, p1, p2, p3, p4):
         def __init__(self, i, p1 = False, p2 = False, p3 = False, p4 = False, vis = False):
             self._vis = vis
             self.i = i
-
-        
+            self.p1 = p1
+            self.p2 = p2
+            self.p3 = p3
+            self.p4 = p4
+    
         def __str__(self):
-            self.i = i if _vis else "X"
-            self.if_p1 = "A" if p1 else " "
-            self.if_p2 = "B" if p2 else " "
-            self.if_p3 = "C" if p3 else " "
-            self.if_p4 = "D" if p4 else " "
+            self.i = i if self._vis else "X"
+            self.if_p1 = "A" if self.p1 else " "
+            self.if_p2 = "B" if self.p2 else " "
+            self.if_p3 = "C" if self.p3 else " "
+            self.if_p4 = "D" if self.p4 else " "
             return self.i, self.if_p1, self.if_p2, self.if_p3, self.if_p4
-        
+    
         @property
         def tile(self, p1 = False, p2 = False, p3 = False, p4 = False, vis = False):
             tile = [i, self.if_p1, self.if_p2, self.if_p3, self.if_p4, self._vis]
@@ -181,7 +135,7 @@ def _(i, lg, locations, p1, p2, p3, p4):
     # plansza = Plansza()
 
     # plansza.pola[1][2].hp = 50
-        
+    
 
     class Locations(object): 
         """game grid"""
@@ -196,10 +150,14 @@ def _(i, lg, locations, p1, p2, p3, p4):
                 for j in loc_letters:
                     locations.append(i+j)
 
-            loci = []
+            tiles = []
+            for num in values:
+                tile = Tile(i = num)
+                tiles.append(tile)
+            self.loci = []
             for num in range(0, 25):
-                loci.append(locations[num], tile = Tile(values[num]))
-                
+                self.loci.append([locations[num], tiles[num]])
+            
         def tile_flip(self, tile):
             tile._vis = True
 
@@ -207,7 +165,7 @@ def _(i, lg, locations, p1, p2, p3, p4):
             for player in self.players:
                 if player.location == tile.loc:
                     tile.p_pnum = True
-            
+        
         def hide_player(self, tile):
             tile.p_pnum = False
 
@@ -219,39 +177,28 @@ def _(i, lg, locations, p1, p2, p3, p4):
                         tile_flip(tile)
                         show_player(tile)
 
-    
-    
-        def grid(self):
-        
-            lc = self.loci
-        
-            print({f"""
-                        |- - - -|- - - -|- - - -|- - - -|- - - -|
-                |{lg('1a')[0]}-    -|{lg('1b')[0]}-    -|{lg('1c')[0]}-    -|{lg('1d')[0]}-    -|{lg('1e')[0]}-    -|
-                |{lg('1a')[1]} {lg('1a')[2]} {lg('1a')[3]} {lg('1a')[4]}|{lg('1b')[1]} {lg('1b')[2]} {lg('1b')[3]} {lg('1b')[4]}|{lg('1c')[1]} {lg('1c')[2]} {lg('1c')[3]} {lg('1c')[4]}|{lg('1d')[1]} {lg('1d')[2]} {lg('1d')[3]} {lg('1d')[4]}|{lg('1e')[1]} {lg('1e')[2]} {lg('1e')[3]} {lg('1e')[4]}|
-                |- - - -|- - - -|- - - -|- - - -|- - - -|
-                |{lg('2a')[0]}-    -|{lg('2b')[0]}-    -|{lg('2c')[0]}-    -|{lg('2d')[0]}-    -|{lg('2e')[0]}-    -|
-                |{lg('2a')[1]} {lg('2a')[2]} {lg('2a')[3]} {lg('2a')[4]}|{lg('2b')[1]} {lg('2b')[2]} {lg('2b')[3]} {lg('2b')[4]}|{lg('2c')[1]} {lg('2c')[2]} {lg('2c')[3]} {lg('2c')[4]}|{lg('2d')[1]} {lg('2d')[2]} {lg('2d')[3]} {lg('2d')[4]}|{lg('2e')[1]} {lg('2e')[2]} {lg('2e')[3]} {lg('2e')[4]}|
-                |- - - -|- - - -|- - - -|- - - -|- - - -|
-                |{lg('3a')[0]}-    -|{lg('3b')[0]}-    -|{lg('3c')[0]}-    -|{lg('3d')[0]}-    -|{lg('3e')[0]}-    -|
-                |{lg('3a')[1]} {lg('3a')[2]} {lg('3a')[3]} {lg('3a')[4]}|{lg('3b')[1]} {lg('3b')[2]} {lg('3b')[3]} {lg('3b')[4]}|{lg('3c')[1]} {lg('3c')[2]} {lg('3c')[3]} {lg('3c')[4]}|{lg('3d')[1]} {lg('3d')[2]} {lg('3d')[3]} {lg('3d')[4]}|{lg('3e')[1]} {lg('3e')[2]} {lg('3e')[3]} {lg('3e')[4]}|
-                |- - - -|- - - -|- - - -|- - - -|- - - -|
-                |{lg('4a')[0]}-    -|{lg('4b')[0]}-    -|{lg('4c')[0]}-    -|{lg('4d')[0]}-    -|{lg('4e')[0]}-    -|
-                |{lg('4a')[1]} {lg('4a')[2]} {lg('4a')[3]} {lg('4a')[4]}|{lg('4b')[1]} {lg('4b')[2]} {lg('4b')[3]} {lg('4b')[4]}|{lg('4c')[1]} {lg('4c')[2]} {lg('4c')[3]} {lg('4c')[4]}|{lg('4d')[1]} {lg('4d')[2]} {lg('4d')[3]} {lg('4d')[4]}|{lg('4e')[1]} {lg('4e')[2]} {lg('4e')[3]} {lg('4e')[4]}|
-                |- - - -|- - - -|- - - -|- - - -|- - - -|
-                |{lg('5a')[0]}-    -|{lg('5b')[0]}-    -|{lg('5c')[0]}-    -|{lg('5d')[0]}-    -|{lg('5e')[0]}-    -|
-                |{lg('5a')[1]} {lg('5a')[2]} {lg('5a')[3]} {lg('5a')[4]}|{lg('5b')[1]} {lg('5b')[2]} {lg('5b')[3]} {lg('5b')[4]}|{lg('5c')[1]} {lg('5c')[2]} {lg('5c')[3]} {lg('5c')[4]}|{lg('5d')[1]} {lg('5d')[2]} {lg('5d')[3]} {lg('5d')[4]}|{lg('5e')[1]} {lg('5e')[2]} {lg('5e')[3]} {lg('5e')[4]}|
-                |- - - -|- - - -|- - - -|- - - -|- - - -|
-            
-            """})
 
+        @property
+        def grid(self):
+    
+            lc = self.loci
+    
+            grid = (f"""
+                |- - - -|- - - -|- - - -|- - - -|- - - -|
+                |{lc[0][0]}-{lc[0][1].i}  -|{lc[1][0]}-{lc[1][1].i}  -|{lc[2][0]}-{lc[2][1].i}  -|{lc[3][0]}-{lc[3][1].i}  -|{lc[4][0]}-{lc[4][1].i}  -| 
+                |{lc[0][1].p1}-{lc[0][1].p2}-{lc[0][1].p3}-{lc[0][1].p4}|
+                |- - - -|- - - -|- - - -|- - - -|- - - -|
+        
+            """)
+                        
+            return grid
 
 
 
     class Game(object): #rewrite all vars!
 
         def __init__(self, players):
-        
+    
             self.locations = Locations()
             self.players = players
 
@@ -267,7 +214,7 @@ def _(i, lg, locations, p1, p2, p3, p4):
 
         def play(self):
             round_count = 0
-            while round_count < 5:
+            while round_count < 2:
 
                 for player in self.players:
                     print(player.name, "points:", player.score)
@@ -275,9 +222,9 @@ def _(i, lg, locations, p1, p2, p3, p4):
                     print(player.location)
 
                 # print(self.players)
-                # print(self.loci.locations)
+                #print(self.locations.loci)
            #     self.player_update()
-                print(locations.grid) #ok
+                print(self.locations.grid) #ok
                 round_count +=1 #ok
 
 
